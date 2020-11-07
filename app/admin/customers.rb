4425e8a11ac9208037e91812bb0d7c24d18a1fa2
Anton Stroy <antosha.stroy@gmail.com>
2020-11-06 23:02:06 -0600

    Added funnctionality to active_admin to upload picturers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Customer do
   permit_params :name, :phone, :email, :notes, :image
+
+  # DSL Dommain Specific language
+  # Formtastic Gem
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
